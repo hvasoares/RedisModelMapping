02d282eaa66f098fae8d08875696795c59042cc0
Hugo Vinícius Alves Soares <hvasoares@gmail.com>
2013-05-29 04:13:05 -0300

    Localization Strategy

---
+++ hashedId/BeforeSaveIdTunning.php
@@ -0,0 +1,37 @@
+<?php
+namespace switch5\domain\hashedId;
+require_once __DIR__.'/../ExtendedRepositoryListenerAdapter.php';
+class BeforeSaveIdTunning extends
+	\switch5\domain\ExtendedRepositoryListenerAdapter{
+	public function __construct($registry){
+		$this->r = $registry;	
+	}
+	public function beforeSave($model){
+		$m = $this->r['Mapper'];		
+		$zexists = $this->r['ZsetExists'];
+		$array = $m->getArray($model);
+		$hash=sha1($this->concatAttrs($array)
+			.$this->r['sha1_salt']
+		);
+		if($zexists->operate(
+			$this->r['zsetkey'],
+			$hash
+		)){
+			$array['id'] = $this->r['Redis']->zscore(
+				$this->r['zsetkey'],
+				$hash
+			);
+			$m->arrayToModel($model,$array);
+		}
+
+		return $model;
+	}
+	public function concatAttrs($array){
+		$attrs = $this->r['attrs'];
+		$result='';
+		foreach($attrs as $attr)
+			$result.=$array[$attr];
+		return $result;
+	}
+}
+?>
---
+++ test/ZsetRemoveOperationTest.php
@@ -0,0 +1,7 @@
+<?php
+namespace switch5\domain;
+require_once 'ZsetRemoveOperation.php';
+class ZsetRemoveOperationTest extends \PHPUnit_Framework_Testcase{
+
+}
+?>
