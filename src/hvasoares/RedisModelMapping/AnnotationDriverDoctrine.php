67859012bac43ad9d1098b810d01a53e5e1ffa9e
Hugo Vinícius Alves Soares <hvasoares@gmail.com>
2013-08-03 00:05:26 -0300

    changing the autoloader in annotationDriverDoctrine to get symfony validator from constant

--- src/hvasoares/RedisModelMapping/AnnotationDriverDoctrine.php
+++ src/hvasoares/RedisModelMapping/AnnotationDriverDoctrine.php
@@ -10,8 +10,20 @@ class AnnotationDriverDoctrine implements AnnotationDriver{
 			$debug = true
 		);
 
+		a\AnnotationRegistry::registerLoader(function($class){
+			$full_dir = 
+				REDIS_MODEL_MAPPING_SYMFONY_VALIDATOR_DIR."/Symfony/Component/Validator/Constraints"
+			if(strpos($class,"\Symfony\Component\Validator\Constraints")===0){
+				$part = explode("\\",$class);
+				$classname = array_pop($part);
+				require_once "$full_dir/$classname.php";
+				require_once "$full_dir/$classname"."Validator.php";
+				return true;
+			}
+			return false;
+		});
+
 		$this->registry = new a\AnnotationRegistry();
-		a\AnnotationRegistry::registerAutoloadNamespace("\\Symfony\\Component\\Validator\\Constraints");
 	}
 	public function getPropertyAnnotations($obj,$propertyName){
 			$obj =new ReflectionObject($obj);
